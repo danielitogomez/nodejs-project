fa8a7009a352d8e6cba777df2adbde089e7e93d8
danielitogomez <danielitogomez.daniel.gomez.amaran@gmail.com>
2020-04-15 00:06:03 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// this will accept all the calls to root URL http://localhost:8080/
+// It will render the index.html available in the Project root directory as a Response
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080');
+});
