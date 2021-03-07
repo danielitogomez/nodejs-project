5256dea0558aaf4071a49ec8a5511fb120bf38ba
danielitogomez <daniel.gomez.amaran@gmail.com>
2021-03-07 16:29:13 -0300

    Add new app

---
+++ web-check-bot/README.md
@@ -0,0 +1,1 @@
+## Web Check Telegram Bot
---
+++ web-check-bot/request.js
@@ -0,0 +1,17 @@
+var axios = require('axios');
+
+var config = {
+  method: 'get',
+  url: 'https://www.helloworld-it.com',
+  headers: { }
+};
+
+axios(config)
+.then(function (response) {
+  console.log(response.status);
+})
+.catch(function (error) {
+  if (error.response) {
+    console.log(error.response.status);
+  }
+});
